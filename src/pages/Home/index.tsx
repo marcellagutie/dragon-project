763172a6c1c7fd05d4bab62bcc0fc2bc84da10d9
Marcella Gutierrez <marcella.gutierrez@hotmail.comm>
2023-02-21 18:12:13 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Card } from "../../components/Card";
+import { Header } from "../../components/Header";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+  );
+}
