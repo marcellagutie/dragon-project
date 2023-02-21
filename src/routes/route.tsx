302d0895810821ef681fdb6060810bd3ccf7b1f0
Marcella Gutierrez <marcella.gutierrez@hotmail.comm>
2023-02-21 18:14:36 -0300

    feat: route file

---
+++ src/routes/route.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { Login } from "../pages/Login";
+import { Home } from "../pages/Home";
+import { Create } from "../pages/Create";
+import { Edit } from "../pages/Edit";
+
+export default function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit/:id" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
